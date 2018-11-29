697fba247e0891d1cb4f9e3b2d2843a70d4da46a
zhuxiao_mac <skypigr@gmail.com>
2018-11-29 00:49:15 -0800

    step 18 add another route: /shopper -> shopper#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'shopper/index'
   resources :products
+  get 'shopper/index'
+  get 'shopper' , to: 'shopper#index'
   root 'shopper#index', as: 'root'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
