89656d59ddddcdbbc4adc6cbd0ff2f9f52b69d45
zhuxiao_mac <skypigr@gmail.com>
2018-11-28 23:37:00 -0800

    create shopper controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'shopper/index'
   resources :products
+  root 'shopper#index', as: 'root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
