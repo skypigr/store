379186841c6c9e15d0dffe1324758a53f990e2c4
zhuxiao_mac <skypigr@gmail.com>
2018-11-29 17:13:02 -0800

    create a migrate to merge duplicated items into one with quantity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_30_004312) do
+ActiveRecord::Schema.define(version: 2018_11_30_010637) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
