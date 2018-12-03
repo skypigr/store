bb84018befe2f09919c25f492bcb4c8274161a65
zhuxiao_mac <skypigr@gmail.com>
2018-12-02 23:17:03 -0800

    create actions to authenticate user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'access/new'
+  get 'access/create'
+  get 'access/destroy'
+  get 'admin/index'
+  get 'admin', to: "admin#index"
+  get 'login', to: "access#new"
   resources :users
   resources :orders
   resources :lineitems
@@ -7,6 +13,7 @@ Rails.application.routes.draw do
   get 'shopper/index'
   get 'shopper' , to: 'shopper#index'
   root 'shopper#index', as: 'root'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
