3b6c340e7e0acd296d8e7c2d583da23556c79751
zhuxiao_mac <skypigr@gmail.com>
2018-12-02 22:33:18 -0800

    complete order related impl

--- app/models/lineitem.rb
+++ app/models/lineitem.rb
@@ -1,7 +1,7 @@
 class Lineitem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
-  belongs_to :order
+  belongs_to :order, optional: true
 
   def total_price
     product.price * quantity
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,12 @@ class Order < ApplicationRecord
     PAYMENT_TYPES = ["Check", "Credit Card", "Venmo", "Paypal"]
 
     validates :name, :address, :email, presence:true
-    validates :paytpye, inclusion:  PAYMENT_TYPES
+    validates :paytype, inclusion:  PAYMENT_TYPES
+
+    def add_items_from_cart(cart)
+        cart.lineitems.each do |item|
+            item.cart_id = nil
+            item.order_id = self.id
+        end
+    end
 end
