cb720e08139fd52708b143367ab4f45451083c83
zhuxiao_mac <skypigr@gmail.com>
2018-12-05 15:12:44 -0800

    add some comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   validates :name, presence:true, uniqueness: true
   has_secure_password
+  # has_secure_password(options = {})
+  # Adds methods to set and authenticate against a BCrypt password. 
+  # This mechanism requires you to have a password_digest attribute.
+  # The following validations are added automatically:
+  #  1.Password must be present on creation
+  #  2.Password length should be less than or equal to 72 bytes
+  #  3.Confirmation of password (using a password_confirmation attribute)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
 
-  get 'admin', to: "admin#index"
-
-  get 'login', to: "access#new"
-  post 'login', to: 'access#create'
-  delete 'logout', to: 'access#destroy'
+  get 'admin', to: "admin#index"      # admin_path
+  get 'login', to: "access#new"       # login_path
+  post 'login', to: 'access#create' 
+  delete 'logout', to: 'access#destroy' # logout
 
   resources :users
   resources :orders
   resources :lineitems
-  # resources :carts, except: [:index]
   resources :carts
   resources :products
   
-  get 'shopper/index'
-  get 'shopper' , to: 'shopper#index'
-  root 'shopper#index', as: 'root'
-
-
+  get 'shopper/index'                 # shopper_index_path
+  get 'shopper' , to: 'shopper#index' # shopper_path
+  root 'shopper#index', as: 'root'    # root_path
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
